b745189200028894dd3f83372dbd37d8ae74fa97
John Hannah <john@friendlymachine.net>
2014-07-22 14:36:12 -0400

    update admin theme  - add scripts.js

---
+++ sites/all/themes/fm_admin/scripts.js
@@ -0,0 +1,23 @@
+(function ($, Drupal, window, document, undefined) {
+  target = jQuery("select#edit-field-commodity-type-und");
+  crop = "";
+  jQuery(target).on('change', function() {
+    crop = jQuery(this).val();
+  switch (crop) {
+    case "almonds" :
+      jQuery( ".walnut-variety, .rice-variety" ).css('display', 'none');
+      jQuery( ".almond-variety" ).css('display', 'block');
+    break;
+  case "walnuts" :
+      jQuery( ".almond-variety, .rice-variety" ).css('display', 'none');
+      jQuery( ".walnut-variety" ).css('display', 'block');
+    break;
+  case "rice" :
+      jQuery( ".almond-variety, .walnut-variety" ).css('display', 'none');
+      jQuery( ".rice-variety" ).css('display', 'block');
+    break;
+  default :
+  jQuery( ".almond-variety, .rice-variety, walnut-variety" ).css('display', 'none');
+  }
+  });
+})(jQuery, Drupal, this, this.document);
